5d695ce825708c128b4dfe138a3a519f8e7e7ac6
Felix Blanchette <felix.blanchette@gmail.com>
2024-10-03 14:29:05 -0400

    pistes possibles ??

--- src/Bot.cs
+++ src/Bot.cs
@@ -2,7 +2,7 @@
 
 public class Bot
 {
-    public const string NAME = "My cool C# bot";
+    public const string NAME = "Singe-O'-Matix=>4DaW1N";
 
     /// <summary>
     /// This method should be use to initialize some variables you will need throughout the game.
@@ -27,6 +27,12 @@ public class Bot
 
         // You can clearly do better than the random actions above. Have fun!!
         var nextMoves = Random.Shared.GetItems(possibleActions, 1);
+
+        // On doit trouver la position des ennemis | les ennemis ont des personnalités différentes : C'est quoi les différences ?
+        // On doit savoir où sont les murs | on peut se déplacer à une position bien précise, comment l'utiliser
+        // On doit connaitre les espaces libres autour de nous
+        // Connaitre le chemin le plus court pour arriver à un ennemi? Je sais pas si sa peut être utile
+        // On doit pouvoir savoir si un ennemi est en vie ou pas    
 
         return nextMoves;
     }
