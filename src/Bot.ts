817d2d44f8f1cb2823a1debe50acf12022a81715
Felix <felix.blanchette@gmail.com>
2024-10-07 13:47:27 -0400

    docs2

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,102 +1,126 @@
-import {Action, ActionType, GameMap, Position, TeamGameState, Threat, TileType} from './GameInterface';
+import { Action, ActionType, GameMap, Position, TeamGameState, Threat, TileType } from './GameInterface';
 
+/**
+ * Bot Class
+ *
+ * Represents a bot that makes decisions in the game based on the current game state.
+ */
 export class Bot {
     constructor() {
         console.log('Initializing your super duper mega bot');
-        // This method should be use to initialize some variables you will need throughout the game.
+        // This constructor initializes any necessary variables for the bot's operation.
     }
 
     /*
-     * Here is where the magic happens, for now the moves are random. I bet you can do better ;)
+     * Determines the next moves for the bot based on the current game state.
+     * Currently, the moves are chosen randomly, but this can be improved with better logic.
+     *
+     * @param gameState - The current state of the game.
+     * @returns An array of actions that the bot will take.
      */
     getNextMoves(gameState: TeamGameState): Action[] {
         const actions: Action[] = [];
-        const mapDimension = getMapDimensions(gameState.map)
-        const map = gameState.map.tiles
-        const threatsPos = getThreatsPos(gameState.threats)
-        const me = gameState.yourCharacter.position
+        const mapDimension = getMapDimensions(gameState.map);
+        const map = gameState.map.tiles;
+        const threatsPos = getThreatsPos(gameState.threats);
+        const me = gameState.yourCharacter.position;
 
-
+        // Possible actions the bot can take.
         const possibleActions: Action[] = [
-            {type: ActionType.MOVE_LEFT},
-            {type: ActionType.MOVE_RIGHT},
-            {type: ActionType.MOVE_UP},
-            {type: ActionType.MOVE_DOWN},
+            { type: ActionType.MOVE_LEFT },
+            { type: ActionType.MOVE_RIGHT },
+            { type: ActionType.MOVE_UP },
+            { type: ActionType.MOVE_DOWN },
         ];
 
+        // Choose a random action from the possible actions.
         actions.push(randomlyChoose(possibleActions));
 
-        // You can clearly do better than the random actions above. Have fun!!
+        // The bot can implement more sophisticated strategies for choosing actions.
         return actions;
     }
 }
 
+/**
+ * Chooses a random element from an array.
+ *
+ * @param arr - The array from which to choose an element.
+ * @returns A randomly selected element from the array.
+ */
 function randomlyChoose<T>(arr: T[]): T {
     return arr[Math.floor(arr.length * Math.random())];
 }
 
 /**
- * return map dimensions
- * * @param gameState
+ * Returns the dimensions of the game map.
+ *
+ * @param map - The game map for which dimensions are to be calculated.
+ * @returns The total number of tiles in the map (width * height).
  */
 const getMapDimensions = (map: GameMap): number => {
-    return map.height * map.width
+    return map.height * map.width;
 }
 
 /**
- * return an array with threats position
- * @param threats
+ * Returns an array of positions representing the locations of threats.
+ *
+ * @param threats - An array of Threat objects.
+ * @returns An array of Position objects representing the locations of each threat.
  */
 const getThreatsPos = (threats: Threat[]): Position[] => {
-    let t = []
+    let t: Position[] = [];
     threats.forEach(threat => {
-        // console.log(threat.position)
-        t.push(threat.position)
-    })
-    return t
+        t.push(threat.position);
+    });
+    return t;
 }
 
 /**
- * return true if a threat is near
- * @param threats = array of threats
- * @param pos = our pos
- * @param danger = danger distance threshold
+ * Checks if any threats are near the specified position.
+ *
+ * @param threats - An array of Threat objects.
+ * @param pos - The current position of the character.
+ * @param danger - The distance threshold for considering a threat to be near.
+ * @returns True if any threat is within the danger distance, otherwise false.
  */
 const isThreatsNear = (threats: Threat[], pos: Position, danger: number): boolean => {
-    let retVal = false
+    let retVal = false;
     threats.forEach(t => {
         if (getDist(pos, t.position) < danger) {
-            retVal = true
+            retVal = true;
         }
-    })
-    return retVal
+    });
+    return retVal;
 }
 
 /**
- * return distance between 2 points
- * @param p1
- * @param p2
+ * Calculates the Manhattan distance between two points.
+ *
+ * @param p1 - The first position.
+ * @param p2 - The second position.
+ * @returns The distance between the two points.
  */
 const getDist = (p1: Position, p2: Position): number => {
-    return Math.abs(p1.x - p2.x) + Math.abs(p2.y - p1.y)
+    return Math.abs(p1.x - p2.x) + Math.abs(p2.y - p1.y);
 }
 
 /**
- * return position x,y from closest threat
- * @param p
- * @param tPos
+ * Finds the closest position to a given point from an array of positions.
+ *
+ * @param p - The reference position.
+ * @param tPos - An array of positions to check against.
+ * @returns The closest position to p from tPos, or null if tPos is empty.
  */
 const closest = (p: Position, tPos: Position[]): Position | null => {
-    let c: Position | null = null
-    let dist: number = Infinity
+    let c: Position | null = null;
+    let dist: number = Infinity;
 
     tPos.forEach(t => {
-        const d: number = getDist(p, t)
+        const d: number = getDist(p, t);
         if (d < dist) {
-            dist = d
-            c = t
+            dist = d;
+            c = t;
         }
-    })
-    return c
+    });
+    return c;
 }
-
